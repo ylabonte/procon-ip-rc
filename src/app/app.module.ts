069895422a837726da9d99c860794a3e1d799b62
Yannic Labonte <yannic.labonte@gmail.com>
2021-09-13 21:08:27 +0200

    Fix lgtm alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
--- src/app/user-menu/user-menu.component.ts
+++ src/app/user-menu/user-menu.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
-import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-user-menu',
